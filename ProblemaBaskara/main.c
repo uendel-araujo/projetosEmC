22dcfbfec121ecad4d7f1f3b03535597f133b5b4
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-22 13:33:27 -0300

    Adicionado o inicio do projeto ProblemaBaskara e a criação das Variaveis

---
+++ ProblemaBaskara/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double a, b, c, delta, x1, x2;
+
+
+    printf("Coeficiente A :");
+    scanf("%lf", a);
+    printf("Coeficiente B :");
+    scanf("%lf", b);
+    printf("Coeficiente C :");
+    scanf("%lf", c);
+
+
+    return 0;
+}
