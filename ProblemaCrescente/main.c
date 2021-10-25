bd115be54bde01ddbcc1ac9b5382452e55315e09
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-25 19:02:46 -0300

    Programa Finalizado

--- ProblemaCrescente/main.c
+++ ProblemaCrescente/main.c
@@ -8,6 +8,17 @@ int main()
     printf("Digite dois numeros: \n");
     scanf("%d %d", &X, &Y);
 
+    while (X != Y) {
+        if (X < Y){
+            printf("CRESCENTE\n");
+        }
+        else {
+            printf("DECRESCENTE\n");
+        }
+
+        printf("Digite outros dois numeros:\n ");
+        scanf("%d %d", &X, &Y);
+    }
 
     return 0;
 }
