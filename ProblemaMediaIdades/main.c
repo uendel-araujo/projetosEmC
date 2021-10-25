12a54a9aa74ac8446234689fdfd9cf2f6b827588
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-25 19:09:09 -0300

    Finalização do Programa

--- ProblemaMediaIdades/main.c
+++ ProblemaMediaIdades/main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int idade, cont, soma;
+    double media;
+
+    printf("Digite as idade: \n");
+    scanf("%d", &idade);
+
+    soma = 0;
+    cont = 0;
+
+    while(idade >= 0) {
+        soma = soma + idade;
+        cont++;
+
+        scanf("%d", &idade);
+    }
+
+    if( cont == 0) {
+        printf("IMPOSSIVEL CALCULAR!");
+    }
+    else {
+        media = (double) soma / cont;
+        printf("MEDIA = %.2lf\n", &media);
+    }
     return 0;
 }
