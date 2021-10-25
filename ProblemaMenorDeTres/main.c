51f29a944ec909b4c9305ae826eaf2fd40c0036f
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-25 18:45:03 -0300

    Criação do arquivo principal e das variaveis primarias e entrada de texto

---
+++ ProblemaMenorDeTres/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c, menor;
+
+    printf("Primeiro Valor: ");
+    scanf("%d",a);
+
+    printf("Segundo Valor: ");
+    scanf("%d",b);
+
+    printf("Terceiro Valor: ");
+    scanf("%d",c);
+
+
+
+    return 0;
+}
