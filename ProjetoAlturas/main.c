fa5c809d3ce96e381eb0b8c843440eda2e62dea8
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-26 19:30:14 -0300

    Criação da lógica e dos FOR's, resta apenas a parte da saída de dados para informação

--- ProjetoAlturas/main.c
+++ ProjetoAlturas/main.c
@@ -4,6 +4,7 @@
 int main()
 {
     int N;
+    double soma, media;
 
     printf("Quantas pessoas serao digitadas? ");
     scanf("%d", &N);
@@ -12,7 +13,25 @@ int main()
     int idades[N];
     double alturas[N];
 
+    for(int i = 0; i < N; i++) {
+        printf("Dados da %dª pessoa \n",i + 1);
+        printf("Nome: ");
+        fseek(stdin, 0, SEEK_END);
+        gets(nomes[i]);
 
+        printf("Idade: ");
+        scanf("%d", &idades[i]);
+
+        printf("Altura: ");
+        scanf("%lf", &alturas[i]);
+    }
+
+    soma = 0;
+    for(int i = 0; i < N; i++) {
+        soma = soma + alturas[i];
+    }
+
+    media = soma / N;
 
     return 0;
 }
