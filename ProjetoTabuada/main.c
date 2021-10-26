f02b452fd630270691c0142888977cc11aef1730
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-25 21:54:52 -0300

    Correção do pulo de linha na impressão dos resultado. Projeto Finalizado

--- ProjetoTabuada/main.c
+++ ProjetoTabuada/main.c
@@ -10,7 +10,7 @@ int main()
 
     for (i = 1; i <= 10; i++) {
       produto = N * i;
-      printf("%d x %d = %d", N , i, produto);
+      printf("%d x %d = %d \n", N , i, produto);
     }
 
     return 0;
