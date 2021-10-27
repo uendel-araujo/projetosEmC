ca3ca8a241408c0deb359eceb7abe4f297fc58b4
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-26 21:17:59 -0300

    Criação do projeto Soma Linhas

---
+++ ProjetoSomaLinhas/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int M, N;
+
+    printf("Qual a quantidade de linhas da Matriz ? ");
+    scanf("%d", &M);
+    printf("Qual a quantidade de colunas da Matriz ? ");
+    scanf("%d", &N);
+
+    return 0;
+}
